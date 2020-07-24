babfdb7d2fb7d38157754a687bede829bda13655
Phoebe <jolinhappy1990@hotmail.com>
2020-07-25 01:29:20 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//mongoose setting
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+
+
 app.get('/', (req, res) => {
   res.send('HIHI')
 })
---
+++ models/restaurant.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+restaurantSchema = new Schema({
+  id: {
+    type: Number,
+    require: false,
+  },
+  name: {
+    type: String,
+    require: false,
+  },
+  name_en: {
+    type: String,
+    require: false,
+  },
+  category: {
+    type: String,
+    require: false,
+  },
+  image: {
+    type: String,
+    require: false,
+  },
+  location: {
+    type: String,
+    require: false,
+  },
+  phone: {
+    type: String
+    require: false,
+  },
+  google_map: {
+    type: String,
+    require: false,
+  },
+  rating: {
+    type: Number,
+    require: false,
+  },
+  description: {
+    type: String,
+    require: false,
+  }
+
+})
+
+module.exports = mongoose.model('Restaurant', restaurantSchema)

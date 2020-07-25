93f7d8323ce58990cc9527d9e432b026f9b0186f
Phoebe <jolinhappy1990@hotmail.com>
2020-07-25 23:14:16 +0800

    feat: add detail page

--- app.js
+++ app.js
@@ -53,6 +53,17 @@ app.post('/restaurants', (req, res) => {
     .catch(error => console.log(error))
 })
 
+
+
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('show', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)
 })

eac64604d1a03d4e9425f2c563ea490831026d12
Phoebe <jolinhappy1990@hotmail.com>
2020-07-26 00:02:20 +0800

    feat: add update function

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }))
 
 
+//瀏覽全部餐廳
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
@@ -36,12 +37,13 @@ app.get('/', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//create new restaurant page
 app.get('/restaurants/new', (req, res) => {
   res.render('new')
 })
 
+//接收新增資料
 app.post('/restaurants', (req, res) => {
-  console.log(req.body)
   const name = req.body.name
   const name_en = req.body.name_en
   const image = req.body.image
@@ -53,8 +55,7 @@ app.post('/restaurants', (req, res) => {
     .catch(error => console.log(error))
 })
 
-
-
+//detail page
 app.get('/restaurants/:id', (req, res) => {
   const id = req.params.id
   return Restaurant.findById(id)
@@ -63,6 +64,44 @@ app.get('/restaurants/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+//edit page
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then(restaurant => res.render('edit', { restaurant }))
+    .catch(error => console.log(error))
+})
+
+//接收修正的餐廳資料
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const image = req.body.image
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const description = req.body.description
+  return Restaurant.findById(id)
+    .then(restaurant => {
+      restaurant.name = name,
+        restaurant.name_en = name_en,
+        restaurant.image = image,
+        restaurant.phone = phone,
+        restaurant.google_map = google_map,
+        restaurant.description = description
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
+})
+
+
+
+
+
+
+//search
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`)

97d3b55f74cf9bf1efb4c7fc8930bda2b1ec1fba
Phoebe <jolinhappy1990@hotmail.com>
2020-07-25 16:25:03 +0800

    feat: add create function

--- app.js
+++ app.js
@@ -24,12 +24,32 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+//bodyparser setting
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true }))
 
 
 app.get('/', (req, res) => {
   Restaurant.find()
     .lean()
     .then(restaurants => res.render('index', { restaurants }))
+    .catch(error => console.log(error))
+})
+
+app.get('/restaurants/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/restaurants', (req, res) => {
+  console.log(req.body)
+  const name = req.body.name
+  const name_en = req.body.name_en
+  const image = req.body.image
+  const phone = req.body.phone
+  const google_map = req.body.google_map
+  const description = req.body.description
+  return Restaurant.create({ name, name_en, image, phone, google_map, description })
+    .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
 
